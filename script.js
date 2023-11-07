ce384288e5396e5e375df58b579706afbebad2cb
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2023-11-07 10:24:22 +0100

    Update Pong

--- script.js
+++ script.js
@@ -1,34 +1,95 @@
 var miCir;
-var posX = 0;
+var posX = 500;
 var velX = 2;
 var velY = 2;
-var posY = 0;
-
+var posY = 300;
+var scorePlayer1 = 0;
+var scorePlayer2 = 0;
+var intervalo;
 window.onload = () => {
+    btnstart = document.getElementById("start");
+    btnstart.addEventListener("click", empezarJuego);
+}
+
+function empezarJuego() {
     miCir = document.getElementById("miCirculo");
     setInterval(mueveCirculo, 1);
     svg = document.querySelector("svg");
-    svg.addEventListener("mousemove", mostrarCoord);
+    player1 = document.getElementById("player1");
+    player2 = document.getElementById("player2");
+    document.addEventListener("keydown", player2KeyDown);
+    document.addEventListener("keyup", player2KeyUp);
+    svg.addEventListener("mousemove", player1FollowMouse);
+    document.getElementById("start").disabled = true;
+
 }
 
-function mueveCirculo(){
-    var cx = parseInt(miCir.getAttribute("cx"));
-    var cy = parseInt(miCir.getAttribute("cy"));
-    if((cx-15)>0 && (cx+15)<1000){
-        actualizarPosX();
-    }
-    else {
-        velX = -1 *velX;
-        actualizarPosX();
-    }
-    if((cy-15)>0 && (cy+15)<600){
-        actualizarPosY();
-    }
-    else {
-        velY = -1 *velY;
-        actualizarPosY();
+var player2Up = false;
+var player2Down = false;
+
+function mueveCirculo() {
+    var cx = posX;
+    var cy = posY;
+    var yplayer1 = parseInt(player1.getAttribute("y"));
+    var yplayer2 = parseInt(player2.getAttribute("y"));
+
+    if (cy < 15 || cy > 585) {
+        velY = -velY;
     }
 
+    if ((cx < 25 && cy >= yplayer1) && cy <= (yplayer1 + 80)) {
+        velX = -velX;
+    }
+
+    if ((cx > 970 && cy >= yplayer2) && cy <= (yplayer2 + 80)) {
+        velX = -velX;
+    }
+
+    posX += velX;
+    posY += velY;
+
+    miCir.setAttribute("cx", posX);
+    miCir.setAttribute("cy", posY);
+
+    if (cx < 0) {
+        posX = 500;
+        posY = 300;
+        miCir.setAttribute("cx", posX);
+        miCir.setAttribute("cy", posY);
+        scorePlayer2++;
+        document.getElementById("scorePlayer2").textContent = scorePlayer2;
+    }
+
+    if (cx > 1000) {
+        posX = 500;
+        posY = 300;
+        miCir.setAttribute("cx", posX);
+        miCir.setAttribute("cy", posY);
+        scorePlayer1++;
+        document.getElementById("scorePlayer1").textContent = scorePlayer1;
+    }
+
+    if (player2Up) {
+        let y = yplayer2 - 5;
+        if (y < 0) {
+            y = 0;
+        }
+        player2.setAttribute("y", y);
+    }
+
+    if (player2Down) {
+        let y = yplayer2 + 5;
+        if (y > 520) {
+            y = 520;
+        }
+        player2.setAttribute("y", y);
+    }
+}
+
+function mostrarGanador() {
+    if(scorePlayer1>=10){
+        
+    }
 }
 
 function actualizarPosX(){
@@ -42,17 +103,31 @@ function actualizarPosY() {
     miCir.setAttribute("cy", posY+velY);
 }
 
-function mostrarCoord(e) {
-    let x = e.clientX;
-    let y = e.clientY;
-    let coordenadas = "Coordenadas: (" + x + "," + y + ")";
-    console.log(coordenadas);
+function player1FollowMouse(e) {
+    let y = e.clientY -100;
+    if (y < 0) {
+        y = 0;
+    }
+    if (y > 520) {
+        y = 520;
+    }
+    player1.setAttribute("y", y);
 }
 
-function player1(e) {
-    let x = e.clientX;
-    let y = e.clientY;
+function player2KeyDown(e) {
+    if (e.key === "Shift") {
+        player2Up = true;
+    }
+    if (e.key === "Control") {
+        player2Down = true;
+    }
+}
 
-    e.clientX = x;
-    e.clientY = y;
+function player2KeyUp(e) {
+    if (e.key === "Shift") {
+        player2Up = false;
+    }
+    if (e.key === "Control") {
+        player2Down = false;
+    }
 }
